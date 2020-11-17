eb5354bc5cb831b67ef8b91569e89f9bca725e35
Gokul Ramakrishnan <ramakrishnan.go@husky.neu.edu>
2020-11-17 02:12:24 -0800

    ui fit finish for chat app

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,11 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.getElementById('message-form');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $sendLocationButton = document.getElementById('share-location');
+
 // server (emit) -> client (receive) -- acknowledgement --> server
 // client (emit) -> server (receive) -- acknowledgement --> client
 
@@ -8,34 +14,48 @@ socket.on('message', (message) => {
 });
 
 function resetFields() {
-	document.getElementById('message-form').elements.message.value = '';
+	$messageFormInput.value = '';
+	$messageFormInput.focus();
 }
 
-document.getElementById('message-form').addEventListener('submit', (event) => {
+$messageForm.addEventListener('submit', (event) => {
 	event.preventDefault();
+
+	// Disable the form here
+	$messageFormButton.setAttribute('disabled', 'disabled');
+
 	// we get event object on all events. event target here is the form itself. by accessing the elements array we get access to all the elements in the form. We can then pick the element of our choice by name is message
 	const message = event.target.elements.message.value;
 
 	// Using ack here
 	// Ack is optional and it always takes the place of the last argument in addition to how many ever data arguments that we want to pass along.
 	socket.emit('sendMessage', message, (error) => {
+		// enable button again
+		$messageFormButton.removeAttribute('disabled');
+		resetFields();
+
 		if (error) {
 			return console.log(error);
 		}
 
 		console.log('Message Delivered!');
 	});
-	resetFields();
 });
 
-document.getElementById('share-location').addEventListener('click', (event) => {
+$sendLocationButton.addEventListener('click', (event) => {
 	event.preventDefault();
 	if (!navigator.geolocation) {
 		return alert("Geolocation isn't supported in this browser");
 	}
 
+	//Disable button here
+	$sendLocationButton.setAttribute('disabled', 'disabled');
+
 	// This still doesn't work with promises.. so we're using callback method
 	navigator.geolocation.getCurrentPosition((position) => {
+		// Re-enable location sharing button
+		$sendLocationButton.removeAttribute('disabled');
+
 		socket.emit(
 			'sendLocation',
 			{ latitude: position.coords.latitude, longitude: position.coords.longitude },
